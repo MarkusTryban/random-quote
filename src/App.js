b8f1963973953c1ced13f996618595e55beb69f7
MarkusTryban <markustryban@gmail.com>
2020-03-21 19:16:49 -0400

    Update App.js with content

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Random Quote Machine</p>
-      </header>
+    <div id='wrapper'>
+      <div id='quote-box'>
+        <div className='quote-text'>
+          <i className='fa fa-quote-left'></i>
+          <span id='text'></span>
+        </div>
+        <div className='quote-author'>
+          <span id='author'></span>
+        </div>
+        <div className='buttons'>
+          <a
+            href='#!'
+            className='button'
+            id='tweet-quote'
+            title='Tweet this quote!'
+            target='_blank'
+          >
+            <i className='fa fa-twitter'></i>
+          </a>
+          <a href='#!' className='button' id='tumblr-quote' target='_blank'>
+            <i className='fa fa-tumblr'></i>
+          </a>
+          <button className='button' id='new-quote'>
+            New quote
+          </button>
+        </div>
+      </div>
+      <div className='footer'>
+        by <a href='https://github.com/MarkusTryban'>Markus Tryban</a>
+      </div>
     </div>
   );
 }
