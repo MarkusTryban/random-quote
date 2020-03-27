24067ea06c47816471c7a6e9bedee1ed30debf02
MarkusTryban <markustryban@gmail.com>
2020-03-26 20:26:58 -0400

    Update getRandomQuote with quoteData

--- src/RandomQuote.js
+++ src/RandomQuote.js
@@ -4,12 +4,7 @@ class RandomQuote extends Component {
   constructor() {
     super();
     this.state = {
-      quote: {
-        content: '',
-        title: '',
-        link: '',
-      },
-      hasQuote: false,
+      quoteData: [],
     };
     this.GET_QUOTE =
       'https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand';
@@ -19,46 +14,31 @@ class RandomQuote extends Component {
     fetch(this.GET_QUOTE)
       .then((res) => res.json())
       .then((result) => {
-        if (result[0].content && result[0].title && result[0].link) {
-          let { quote } = this.state;
-          let quoteData = result[0];
-          quote.content = quoteData.content;
-          quote.title = quoteData.title;
-          quote.link = quoteData.link;
-          this.setState({ quote }, () => {
-            if (this.state.hasQuote === false) {
-              this.setState({ hasQuote: true });
-            }
-          });
-        } else {
-          return console.error('No Quote Found');
-        }
+        this.setState({
+          quoteData: result,
+        });
         // result[Math.floor(Math.random() * result.length)],
       });
   };
 
-  renderQuote = () => {
-    const { content, title } = this.state.quote;
-
-    return (
-      <div>
-        <h1>{title}</h1>
-        <p>{content}</p>
-      </div>
-    );
-  };
-
   render() {
-    const { hasQuote } = this.state;
+    const { quoteData } = this.state;
 
     return (
       <Fragment>
         <h1>Random Quote</h1>
         <button onClick={this.getRandomQuote}>New Quote</button>
         <br />
-        {hasQuote === true ? this.renderQuote() : 'No Quote'}
+        <ul>
+          {quoteData.map((data) => (
+            <li>
+              {data.content} {data.title}
+            </li>
+          ))}
+        </ul>
       </Fragment>
     );
   }
 }
+
 export default RandomQuote;
