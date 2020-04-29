9227d319c1e4a6e6dc08c8370e02bacfb3d91782
MarkusTryban <markustryban@gmail.com>
2020-04-28 20:26:48 -0400

    App cleanup

--- src/RandomQuote.js
+++ src/RandomQuote.js
@@ -49,66 +49,64 @@ class RandomQuote extends Component {
 
     return (
       <Fragment>
-        <div id='wrapper'>
-          <div id='quote-box'>
-            <div className='quote-text'>
-              <i className='fa fa-quote-left'></i>
-              {quoteData.map((data) => (
-                <span id='text' key={data.quote}>
-                  {data.quote}
-                </span>
-              ))}
-            </div>
-            <div className='quote-author'>
-              {quoteData.map((data) => (
-                <span id='author' key={data.character}>
-                  - {data.character}
-                </span>
-              ))}
-            </div>
-            <div className='buttons'>
-              <a
-                href='https://twitter.com/intent/tweet'
-                className='button'
-                id='tweet-quote'
-                title='Tweet this quote!'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <i className='fab fa-twitter'></i>
-              </a>
-              <a
-                href='https://www.tumblr.com/login'
-                className='button'
-                id='tumblr-quote'
-                title='Post this quote on tumblr!'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <i className='fab fa-tumblr'></i>
-              </a>
-              <button
-                className='button'
-                id='new-quote'
-                onClick={() => {
-                  this.getRandomQuote();
-                  this.randomColor();
-                }}
-              >
-                New Quote
-              </button>
-            </div>
+        <div id='quote-box'>
+          <div className='quote-text'>
+            <i className='fa fa-quote-left'></i>
+            {quoteData.map((data) => (
+              <span id='text' key={data.quote}>
+                {data.quote}
+              </span>
+            ))}
           </div>
-          <div className='footer'>
-            by{' '}
+          <div className='quote-author'>
+            {quoteData.map((data) => (
+              <span id='author' key={data.character}>
+                - {data.character}
+              </span>
+            ))}
+          </div>
+          <div className='buttons'>
             <a
-              href='https://markustryban.com'
+              href='https://twitter.com/intent/tweet'
+              className='button'
+              id='tweet-quote'
+              title='Tweet this quote!'
               target='_blank'
               rel='noopener noreferrer'
             >
-              Markus Tryban
+              <i className='fab fa-twitter'></i>
             </a>
+            <a
+              href='https://www.tumblr.com/login'
+              className='button'
+              id='tumblr-quote'
+              title='Post this quote on tumblr!'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fab fa-tumblr'></i>
+            </a>
+            <button
+              className='button'
+              id='new-quote'
+              onClick={() => {
+                this.getRandomQuote();
+                this.randomColor();
+              }}
+            >
+              New Quote
+            </button>
           </div>
+        </div>
+        <div className='footer'>
+          by{' '}
+          <a
+            href='https://markustryban.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Markus Tryban
+          </a>
         </div>
       </Fragment>
     );
