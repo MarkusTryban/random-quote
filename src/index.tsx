af2cd9c72b38cd892b2c71f3f335a8eb39d67b19
MarkusTryban <markustryban@gmail.com>
2020-09-21 10:57:47 -0400

    Fix typo

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 
 import RandomQuote from './components/RandomQuote/RandomQuote';
 
-ReactDOM.render(
-  <RandomQuote />
-    document.getElementById('root')
-);
+ReactDOM.render(<RandomQuote />, document.getElementById('root'));
