ae24e13dbccbd49ee3176a9054325c590f6479dc
MarkusTryban <markustryban@gmail.com>
2020-09-21 10:56:39 -0400

    Change index to index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RandomQuote from './components/RandomQuote/RandomQuote';
+
+ReactDOM.render(
+  <RandomQuote />
+    document.getElementById('root')
+);
