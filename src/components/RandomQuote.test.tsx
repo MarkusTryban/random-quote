a9982b4335d9537d5432c547e2ee43c28cb291af
MarkusTryban <markustryban@gmail.com>
2020-09-29 14:51:37 -0400

    Should return portfolio link

--- src/components/RandomQuote.test.tsx
+++ src/components/RandomQuote.test.tsx
@@ -7,3 +7,7 @@ import RandomQuote from './RandomQuote';
 test('should render without crashing', () => {
   render(<RandomQuote />);
 });
+test('should return portfolio link', () => {
+  render(<RandomQuote />);
+  expect(screen.getByRole('link', { name: /markus tryban/i }));
+});
